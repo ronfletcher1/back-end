4be56b22614fe77331230a36058f2219b6b67a32
ronfletcher1 <ronfletcher1@me.com>
2019-02-20 13:53:09 -0500

    updated index for to do list edit

--- routes/index.js
+++ routes/index.js
@@ -25,5 +25,35 @@ router.post('/addTask',(req, res)=>{
     });
 });
 
+router.get('/getTasks', (req, res)=>{
+  const getTasksQuery = `SELECT * FROM tasks`;
+  connection.query(getTasksQuery,(error2,results2)=>{
+      if(error2){throw error2};
+      res.json(results2)
+    })
+})
+
+router.get('/getTask/:tid', (req, res)=>{
+   const tid = req.params.tid;
+   const selectTaskQuery = `SELECT * FROM tasks WHERE id = ?`;
+        connection.query(selectTaskQuery,[tid],(err, result)=>{
+            if(err){throw err}
+            res.json({task: result[0]});
+        })
+})
+
+router.post('/edit', (req, res)=>{
+  const id = req.body.id;
+  const taskName = req.body.task.taskName
+  const taskDate = req.body.task.taskDate.substring(0,10);
+  const updateQuery = `UPDATE tasks SET taskName = ?, taskDate= ?
+  WHERE id = ?`;
+  connection.query(updateQuery,[taskName,taskDate, id],(error,results)=>{
+    if(error){throw error}
+      res.json({
+        msg: "updated"
+      })
+    })
+  });
 
 module.exports = router;
